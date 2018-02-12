ae97b0c397336c79228744051be335012d0257ba
Lluvia Frias <lluvi.frias@gmail.com>
2018-02-12 23:17:40 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+
+def position_taken?(board, index)
+
+  if  board[0] == "X" && board [8] == "O"
+  true
+
+  else board[0] == " " || "" || nil
+    false
+  end
+
+end
