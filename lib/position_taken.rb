411c40ef5b5616d55c97c33b5bc0ce9264765671
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-05 22:21:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,29 @@
-
 def position_taken?(board, index)
-
-  if  board[0] == "X" && board [8] == "O"
-  true
-
-  else board[0] == " " || "" || nil
+  if board[index] == " " || board[index] == "" || board[index] == nil
     false
-  end
+  else
+    true
 
 end
+
+
+end
+
+
+
+
+
+
+
+
+
+# def position_taken?(board, index)
+#
+#   if  board[0] == "X" && board [8] == "O"
+#   true
+#
+#   else board[0] == " " || "" || nil
+#     false
+#   end
+#
+# end
